b67ebc74377d6980db01ac4c362c9de40cac8e76
iKhwezi App <konastonetech@gmail.com>
2026-07-08 21:58:41 +0300

    Simplify design-system exports

--- frontend-v3/src/design-system/index.js
+++ frontend-v3/src/design-system/index.js
@@ -8,26 +8,7 @@
  * import { colors, typography, spacing, animations } from '@/design-system'
  */
 
-export { colors as colors, default as defaultColors } from './colors'
-export { typography } from './typography'
-export { spacing, sizing, borders, shadows } from './spacing'
-export { transitions, animations, keyframes } from './motion'
-
-// Design Tokens Export with ES6 imports
-import * as colorsModule from './colors'
-import { typography as typographyModule } from './typography'
-import { spacing as spacingModule, sizing as sizingModule, borders as bordersModule, shadows as shadowsModule } from './spacing'
-import { transitions as transitionsModule, animations as animationsModule, keyframes as keyframesModule } from './motion'
-
-export const designTokens = {
-  colors: colorsModule.default,
-  typography: typographyModule,
-  spacing: spacingModule,
-  motion: {
-    transitions: transitionsModule,
-    animations: animationsModule,
-    keyframes: keyframesModule
-  }
-}
-
-export default designTokens
+export { colors, default as defaultColors } from './colors.js'
+export { typography } from './typography.js'
+export { spacing, sizing, borders, shadows } from './spacing.js'
+export { transitions, animations, keyframes } from './motion.js'
